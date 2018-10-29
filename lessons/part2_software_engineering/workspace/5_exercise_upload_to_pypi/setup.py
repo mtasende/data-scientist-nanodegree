fcd8b8b27993b17277ef925c451acb8e45ca0d04
Miguel Tasende <miguel.tasende@gmail.com>
2018-10-29 11:04:59 -0300

    chore: moving folders

--- lessons/part2_software_engineering/workspace/5_exercise_upload_to_pypi/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='dsnd_mt_distributions',
-      version='0.1',
-      description='Gaussian and Binomial distributions',
-      packages=['dsnd_mt_distributions'],
-      zip_safe=False)
