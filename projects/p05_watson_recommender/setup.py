61dd80e7df119bfa06c068903308bd11508da3fc
Miguel Tasende <miguel.tasende@gmail.com>
2018-12-06 17:29:09 -0300

    continue recommenders project notebook

---
+++ projects/p05_watson_recommender/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='watson-recommender',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Watson Recommender',
+    author='Miguel Tasende',
+    license='MIT',
+)
